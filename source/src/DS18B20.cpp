94810c6dc7e008f085e7983ecd8b9dfcf6531bf7
Sietse <sietsejongsma@hotmail.com>
2023-06-24 21:49:06 +0200

    Extended/ improved the logging.

--- source/src/DS18B20.cpp
+++ source/src/DS18B20.cpp
@@ -1,9 +1,70 @@
 
+
+#include <cstdio>
+#include <cmath>
+
+
+
+
 #include "codal-core/inc/driver-models/Timer.h"
 #include "DS18B20.h"
 
 #include "utils.h"
 
+namespace {
+
+void update_sample(void *vp_sensor) {
+
+  const shj::DS18B20 *sensor = ((shj::DS18B20 *) vp_sensor);
+
+ // sensor->update_sample(); // <== Function to be implemented
+
+  // Return
+  return;
+}
+
+std::string double_to_string(const double &x) {
+
+  double val(x);
+
+  // Determine the number of digits
+  int digits = 1;
+  if (x >= 1)
+    digits = static_cast<int>(std::log10(x)) + 1;
+
+  // Compute the base
+  double base = 1;
+  int tmp = digits - 1;
+  while (tmp > 0) {
+    base *= 10;
+    --tmp;
+  }
+
+  // Compose the string
+  std::string result;
+  while (base >= 1) {
+    int D = val / base;
+    result += static_cast<char>(D + 48);
+
+    val -= base * D;
+    base /= 10;
+  }
+
+  result += '.';
+
+  // Add the first decimal digit
+  val *= 10;
+
+  // Note, that some rounding down has been observe here
+  result += static_cast<char>(static_cast<int>(val) + 48);
+
+  // Return the result
+  return result;
+}
+
+
+
+} // Closing brace for anonymous namespace
 
 namespace shj {
 
@@ -36,6 +97,11 @@ double DS18B20::get_temperature() const {
 
   m_logger->debug("DS18B20::get_temperature ~ Obtaining new temperature from sensor.");
 
+  // Update the sample in a different fiber
+  //create_fiber(::update_sample, (void *) this);
+
+  // Update sample function from here
+
   // We need to obtain a new temperature from the sensor
   start();
   sleep_us(100);
@@ -52,8 +118,16 @@ double DS18B20::get_temperature() const {
   // Read the information
   unsigned int TL = m_one_wire.read_byte();
   //uBit.serial.printf("TL=%d\r\n",TL);
+
+  char buffer[50];
+  int nchar = sprintf(buffer, "TL = %d.", TL);
+  m_logger->debug("DS18B20::get_temperature ~ Read byte: " + std::string(buffer));
+
   sleep_us(100);
   unsigned int TH = m_one_wire.read_byte();
+
+  nchar = sprintf(buffer, "TH = %d.", TH);
+  m_logger->debug("DS18B20::get_temperature ~ Read byte: " + std::string(buffer));
   //uBit.serial.printf("TH=%d\r\n",TH);
 
   unsigned int temp = TH;
@@ -68,6 +142,7 @@ double DS18B20::get_temperature() const {
 		result = temp * 0.0625; // Resolutie nog instellen
 	}
 
+  m_logger->debug("DS18B20::get_temperature ~ Got result: " + ::double_to_string(result) + " C.");
   // Don't forget to update the cache values:
 
   // Update the time of the last read
